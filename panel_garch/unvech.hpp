99d5afc0dee3efd87fab3c8586e11a36006144f7
Khdbble <fb503d52.af1c4d@gmail.com>
2020-12-06 19:28:35 +0900

    debugged accessing of an element

--- panel_garch/unvech.hpp
+++ panel_garch/unvech.hpp
@@ -3,15 +3,13 @@
 #include <cmath>
 #include <vector>
 #include <cassert>
-#include <panel_garch/matrix>
-
-using panel_garch::Matrix;
+#include "panel_garch/matrix"
 
 // unvech column vector to a Toeplitz matrix
 template <typename T>
 Matrix<T> unvech(const Matrix<T>& V) {
     int writer = 0;
-    assert(("unvech on matrix.", V.c != 1));
+    assert(("unvech on matrix.", V.c == 1));
     int d = (-1.0 + sqrt(1 + 8 * V.r)) / 2;
 
     // validate vector size
@@ -20,8 +18,8 @@ Matrix<T> unvech(const Matrix<T>& V) {
     Matrix<T> res(d, d);
     for (int j = 0; j < d; ++j) {
         for (int i = j; i < d; ++i) {
-            res[i][j] = V[writer];
-            res[j][i] = V[writer];
+            res[i][j] = V[writer][0];
+            res[j][i] = V[writer][0];
             writer++;
         }
     }
