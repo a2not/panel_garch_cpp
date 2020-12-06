f4f83342ead6d6a5c7a6a920397be4d84d61d68f
Khdbble <fb503d52.af1c4d@gmail.com>
2020-12-06 19:27:38 +0900

    use Matrix struct instead of Eigen

--- panel_garch/vech.hpp
+++ panel_garch/vech.hpp
@@ -1,25 +1,24 @@
 #pragma once
 
 #include <cassert>
-#include <Eigen/Core>
-
-using Eigen::Matrix;
+#include "panel_garch/matrix"
 
 // take vech operation on d*d symmetric matrix and return (d*(d+1) / 2) dimension column std::vector
-template<typename T, int d>
-Matrix<T, (int)(d * (d + 1)) / 2, 1> vech(Matrix<T, d, d> M) {
+template<typename T>
+Matrix<T> vech(const Matrix<T> &M) {
     // validate M is symmetric
-    for (int i = 0; i < d; ++i) {
-        for (int j = 0; j < d; ++j) {
-            assert(("vech on non-symmetric matrix.", M(i, j) == M(j, i)));
+    assert(("vech on non-square matrix", M.r == M.c));
+    for (int i = 0; i < M.r; ++i) {
+        for (int j = 0; j < M.c; ++j) {
+            assert(("vech on non-symmetric matrix.", M[i][j] == M[j][i]));
         }
     }
 
     int writer = 0;
-    Matrix<double, (int)(d * (d + 1)) / 2, 1> res;
-    for (int j = 0; j < d; ++j) {
-        for (int i = j; i < d; ++i) {
-            res(writer++, 0) = M(i, j);
+    Matrix<T> res((M.r * (M.r + 1)) / 2, 1);
+    for (int j = 0; j < M.r; ++j) {
+        for (int i = j; i < M.c; ++i) {
+            res[writer++][0] = M[i][j];
         }
     }
     return res;
